a1b785ad75c938bb6222f69fc2ebd8c5ec3c5c8b
Jakub Erazmus <47739109+jerazmus@users.noreply.github.com>
2023-05-11 16:28:14 +0200

    Some changes

--- Automation/Automation.Core/Utilities/XPath.cs
+++ Automation/Automation.Core/Utilities/XPath.cs
@@ -4,22 +4,16 @@
     {
         // Returns XPath with containing text, with additional parameter for double quoatation in text
         public static string TextContains(string text, bool doubleQuotation = false)
-        {
-            return doubleQuotation
+            => doubleQuotation
                 ? $"//*[contains(.,\"{text}\")]"
                 : $"//*[contains(.,'{text}')]";
-        }
 
         // Returns XPath based on data-test name or ID
         public static string Element(string name)
-        {
-            return $"//*[@data-test='{name}' or @id='{name}']";
-        }
+            => $"//*[@data-test='{name}' or @id='{name}']";
 
         // Returns XPath based on class
         public static string ElementClass(string className)
-        {
-            return $"//*[contains(@class, '{className}')]";
-        }
+            => $"//*[contains(@class, '{className}')]";
     }
 }
