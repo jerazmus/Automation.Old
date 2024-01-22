9ea75213506e53684ac2936d1cdaf3cbbee00b4b
Jakub Erazmus <47739109+jerazmus@users.noreply.github.com>
2024-01-22 19:26:36 +0100

    Insert test data into table while creating database

--- Application/Application.Database/Dapper/DapperRepository.cs
+++ Application/Application.Database/Dapper/DapperRepository.cs
@@ -42,7 +42,10 @@ namespace Application.Database.Dapper
 	                        Name varchar(255),
 	                        Surname varchar(255),
 	                        Email varchar(255),
-                        )";
+                        );
+                        INSERT INTO [dbo].[User] VALUES ('58e8974b-c358-4c38-b4d2-743b03d90045', 'TEST1', 'test1', 'email1@email.com');
+                        INSERT INTO [dbo].[User] VALUES ('247dcd8a-ea88-4b1f-ad25-3cbb98e00587', 'TEST2', 'test2', 'email2@email.com');
+                        INSERT INTO [dbo].[User] VALUES ('847f863c-543e-4ca6-99d9-8e750e080ee2', 'TEST3', 'test3', 'email3@email.com')";
             using (_appConnection)
             {
                 foreach (var statement in sql.Split(";"))
